8a6c86973ea6c9c4c42b3573cb5f6dbbe3133665
IrinaT <irina.tiora@isa.utm.md>
2022-09-28 14:00:34 +0300

    Add third checkpoint

--- kitchen-elem/config.go
+++ kitchen-elem/config.go
@@ -5,7 +5,21 @@ import (
 )
 
 const (
-	TimeUnit = time.Duration(float64(time.Millisecond) * 50)
+	TimeUnit = time.Duration(float64(time.Millisecond) * 10)
 	//URL      = "http://dinning-hall:8082/distribution"
 	URL = "http://localhost:8082/distribution"
+)
+
+type apparatusType string
+
+const (
+	stoveLit apparatusType = "oven"
+	ovenLit  apparatusType = "stove"
+)
+
+var (
+	Ovens  Apparatus
+	Stoves Apparatus
+	//used for priority
+	CookFree = make(chan int, 11)
 )
--- kitchen-elem/cookingapparatus.go
+++ kitchen-elem/cookingapparatus.go
@@ -1,1 +1,55 @@
 package kitchen_elem
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+type Apparatus struct {
+	Name      apparatusType `json:"name"`
+	Quantity  int           `json:"quantity"`
+	QuantChan chan int
+}
+
+func (a Apparatus) cookFood(food FoodToCook) {
+	a.QuantChan <- 1
+	time.Sleep(TimeUnit * time.Duration(Foods[food.FoodId-1].PreparationTime))
+	<-a.QuantChan
+	food.Wg.Done()
+
+}
+
+type apparatuses struct {
+	ApparatusList []Apparatus `json:"apparatuses"`
+}
+
+func InitiateApparatus() {
+
+	file, err := os.Open("kitchen-elem/jsonConfig/apparatus.json")
+	if err != nil {
+		log.Fatal("Error opening apparatus.json", err)
+	}
+	defer file.Close()
+	byteValue, _ := io.ReadAll(file)
+	var apparList apparatuses
+	err = json.Unmarshal(byteValue, &apparList)
+	if err != nil {
+		log.Fatal("Error unmarshling apparatus.json", err)
+	}
+
+	for i := range apparList.ApparatusList {
+
+		if apparList.ApparatusList[i].Name == stoveLit {
+			Stoves = apparList.ApparatusList[i]
+			Stoves.QuantChan = make(chan int, apparList.ApparatusList[i].Quantity)
+
+		} else if apparList.ApparatusList[i].Name == ovenLit {
+			Ovens = apparList.ApparatusList[i]
+			Ovens.QuantChan = make(chan int, apparList.ApparatusList[i].Quantity)
+
+		}
+	}
+}
